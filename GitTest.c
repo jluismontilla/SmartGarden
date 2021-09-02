d27305e7d6d6c97ae2d6456aad69ea9e28e0e593
jluismontilla <jluismontillam@gmail.com>
2021-09-02 13:01:10 -0400

    sum

--- GitTest.c
+++ GitTest.c
@@ -2,6 +2,16 @@
 
 int main () {
 
-    printf("Hello World!!!");
+    int i,j;
+    int sum;
+
+    printf("Enter first number: \n");
+        scanf("%d",&i);
+    printf("Enter second number: \n");
+        scanf("%d",&j);
+
+    sum = i + j;
+
+    printf("The sum of %d + %d = %d \n", i, j, sum);
 
 }
